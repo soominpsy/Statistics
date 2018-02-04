5cad28a4c84779049ff07b46a639f5d7440495bc
junholeephy <skyblue1293@naver.com>
2018-02-05 00:57:18 +0100

    pmf & pdf of chapter_02

---
+++ chapter_02/n4_exponential_distribution.py
@@ -0,0 +1,37 @@
+import numpy as np
+from scipy.stats import expon
+from matplotlib import pyplot as plt
+
+# Define the distribution parameters to be plotted
+theta_values = [1]
+linestyles = ['-']
+Color = ['black']
+#------------------------------------------------------------
+# plot the distributions
+#   we generate it using scipy.stats.poisson().  Once the distribution
+#   object is created, we have many options: for example
+#   - dist.pmf(x) evaluates the probability mass function in the case of
+#     discrete distributions.
+#   - dist.pdf(x) evaluates the probability density function for
+#   evaluates
+#fig, ax = plt.subplots(figsize=(5, 3.75))
+
+for THETA, ls, COLOR in zip(theta_values, linestyles, Color):
+    # create a poisson distribution
+    # we could generate a random sample from this distribution using, e.g.
+    #   rand = dist.rvs(1000)
+    dist = expon()
+    x = np.arange(-1, 200)
+
+    plt.plot(x, dist.pdf(x), ls=ls, color=COLOR,
+             label=r'$\theta=%i$' % THETA, linestyle='steps-mid')
+
+plt.xlim(-0.5, 30)
+plt.ylim(0, 0.4)
+
+plt.xlabel('$x$')
+plt.ylabel(r'$p(x|\mu)$')
+plt.title('Poisson Distribution')
+
+plt.legend()
+plt.show()
