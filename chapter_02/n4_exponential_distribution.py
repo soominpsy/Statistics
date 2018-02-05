2bd28dac43784c79c65ebdb1dbfb8d8fa98da168
junholeephy <skyblue1293@naver.com>
2018-02-05 01:30:17 +0100

    multiple exponential distributions

--- chapter_02/n4_exponential_distribution.py
+++ chapter_02/n4_exponential_distribution.py
@@ -3,9 +3,9 @@ from scipy.stats import expon
 from matplotlib import pyplot as plt
 
 # Define the distribution parameters to be plotted
-theta_values = [1]
-linestyles = ['-']
-Color = ['black']
+theta_values = [1,5,10]
+linestyles = ['-','--',':']
+Color = ['black','blue','red']
 #------------------------------------------------------------
 # plot the distributions
 #   we generate it using scipy.stats.poisson().  Once the distribution
@@ -20,7 +20,7 @@ for THETA, ls, COLOR in zip(theta_values, linestyles, Color):
     # create a poisson distribution
     # we could generate a random sample from this distribution using, e.g.
     #   rand = dist.rvs(1000)
-    dist = expon()
+    dist = expon(0,THETA)
     x = np.arange(-1, 200)
 
     plt.plot(x, dist.pdf(x), ls=ls, color=COLOR,
